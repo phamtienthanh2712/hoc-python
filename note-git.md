45d60592e6e661f2b4d0dd6c14873b3eb340f3da
phamtienthanh2712 <pt913136@gmail.com>
2021-02-13 22:00:37 +0700

    ghi chú để nhớ cách đẩy code lên git..

---
+++ note-git.md
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Mô tả những gì vừa làm, thao tác với code"
+git push
--- person.py
+++ person.py
@@ -1,3 +1,4 @@
 # Đề bài: Nhập vào số tuổi (age), rồi in ra xem người đó là người lớn hay trẻ con
 # Nếu là người lớn thì tuổi > 18 (age >= 18)
 # Còn trẻ con thì dưới 18 (18 < age)
+
